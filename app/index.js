f780a22c43043765f836f3bd8cc4b5cd571a5619
LH <l.holota@outlook.com>
2016-05-26 10:36:38 +0200

    Elements renamed, splitter initial width set

--- app/index.js
+++ app/index.js
@@ -9,8 +9,8 @@ document.addEventListener("keydown", function (e) {
     });
 
 jQuery(function ($) {
-	$('#widget').split({ orientation: 'vertical', limit: 100 });
-	$('#foo').split({ orientation: 'horizontal', limit: 10 });
-	$('#a').split({ orientation: 'vertical', limit: 10 });
+	$('#main').split({ orientation: 'vertical', limit: 250, position: '25%' });
+	//$('#foo').split({ orientation: 'horizontal', limit: 10 });
+	//$('#a').split({ orientation: 'vertical', limit: 10 });
 	// $('#spliter2').css({ width: 200, height: 300 }).split({ orientation: 'horizontal', limit: 20 });
 });
